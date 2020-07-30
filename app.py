5fa3a4e708db39f8efe732a0df0ad819f264d4af
erhandemirbas <58423364+erhandemirbas@users.noreply.github.com>
2020-07-30 18:17:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
+import logging
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
-import logging
 
 import pandas as pd
 from sklearn.externals import joblib
@@ -13,7 +13,6 @@ LOG.setLevel(logging.INFO)
 
 def scale(payload):
     """Scales Payload"""
-    
     LOG.info(f"Scaling Payload: \n{payload}")
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
